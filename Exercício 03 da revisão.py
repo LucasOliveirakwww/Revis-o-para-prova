a6914df9acfc2ec7c50f5fcbe81e071caf897ed8
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-04-22 15:12:03 -0300

    Exercício 03 da revisão

---
+++ Exercício 03 da revisão.py
@@ -0,0 +1,5 @@
+#Receber número inteiro e imprimir antecessor e sucessor
+receb = int(input("Digite um número: "))
+Suce = receb + 1
+Ante = receb - 1
+print (f"O número é {receb}, o antecessor é {Ante}, e o sucessor é {Suce}")
