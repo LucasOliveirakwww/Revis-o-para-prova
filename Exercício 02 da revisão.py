e3eb215fcb65c07beff7c2323ef55437c19115b7
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-04-22 15:07:54 -0300

    Alteração do exercício 02 da revisão

--- Exercício 02 da revisão.py
+++ Exercício 02 da revisão.py
@@ -1,11 +1,14 @@
 #Fazer algorítmo para receber um número e imprimir se é par/ímpar, positivo/negativo
-entrada = int(input ("Digite um número:"))
-if entrada > 0 and entrada % 2 == 0:
-    print(f"O número {entrada} é positivo e par")
-elif entrada < 0 and entrada % 2 == 0:
-    print(f"O número {entrada} é negativo e par")
-elif entrada < 0 and entrada % 2 != 0:
-    print(f"O número {entrada} é negativo e ímpar")
-else:
-    print(f"O número {entrada} é positivo e ímpar")
+novo = "s"
+while novo == "s":
+    entrada = int(input ("Digite um número:"))
+    if entrada > 0 and entrada % 2 == 0:
+        print(f"O número {entrada} é positivo e par")
+    elif entrada < 0 and entrada % 2 == 0:
+        print(f"O número {entrada} é negativo e par")
+    elif entrada < 0 and entrada % 2 != 0:
+        print(f"O número {entrada} é negativo e ímpar")
+    elif entrada >0 and entrada % 2 != 0:
+        print(f"O número {entrada} é positivo e ímpar")
+    novo = input("deseja repetir? ")
 
