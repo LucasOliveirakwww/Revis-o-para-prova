6c763a86843f41b0f1788aefede784823a12854b
Lucas Oliveira <lucasoliveiraskkwww@gmail.com>
2025-04-22 16:20:40 -0300

    correção, else, exercício 05 da revisão

--- Exercício 05 da revisão.py
+++ Exercício 05 da revisão.py
@@ -4,13 +4,13 @@ altura = float(input("Digite sua altura (utilize . para vírgula): "))
 IMC = peso/(altura)**2
 if IMC < 18.6:
     print("Você está abaixo do peso")
-if IMC > 18.6 and IMC < 24.9:
+elif IMC > 18.6 and IMC < 24.9:
     print("Peso ideal (parabéns)")
-if IMC > 25.0 and IMC < 29.9:
+elif IMC > 25.0 and IMC < 29.9:
     print("Levemente acima do peso")
-if IMC > 30.0 and IMC < 34.9:
+elif IMC > 30.0 and IMC < 34.9:
     print("Obesidade Grau I")
-if IMC > 35.0 and IMC < 39.9:
+elif IMC > 35.0 and IMC < 39.9:
     print("Obesidade Grau II (Severa)")
-if IMC >= 40:
+else:
     print("Obesidade Grau III (mórbida)")
